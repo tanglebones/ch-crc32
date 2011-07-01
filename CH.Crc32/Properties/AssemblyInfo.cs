d060c187db43118b3438c895b935ad67232fbfab
Cliff Hammerschmidt <cliff@hammerschmidt.ca>
2011-07-01 11:05:46 -0700

    Inital Version

---
+++ CH.Crc32/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CH.Crc32")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clifford Hammerschmidt")]
+[assembly: AssemblyProduct("CH.Crc32")]
+[assembly: AssemblyCopyright("Copyright © 2011 Clifford Hammerschmidt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("83e34103-6604-48d9-acef-2220e98c71f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
